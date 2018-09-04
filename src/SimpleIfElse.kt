ccb9bf64278c2494ca41985310efeae09ff66ab0
Gerbi <gerbi@protonmail.com>
2018-09-03 19:57:48 -0700

    ifElse

---
+++ src/SimpleIfElse.kt
@@ -0,0 +1,18 @@
+fun main(args:Array<String>){
+
+    println("enter your grade: ")
+    var grade= readLine()!!.toDouble()
+
+    if (grade>=90){
+        //block of code
+        println("You ares in A level")
+
+    }else{
+        //block of code
+        println("You ares not in A level")
+    }
+
+
+    println("Youy enter grade $grade")
+
+}
