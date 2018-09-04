b52c5eadb93b0f3793f7ff454508eada37b469be
Gerbi <gerbi@protonmail.com>
2018-09-03 20:11:04 -0700

    When

---
+++ src/SimpleWhen.kt
@@ -0,0 +1,18 @@
+fun main(args:Array<String>){
+
+    var x=30;
+
+    when(x) {
+
+        1,2 -> {
+            print("value is 1 or 2")
+        }
+        in 10..30 -> {
+            print("value in range 10..30")
+        }!in 10..30 -> {
+        print("value is not range 10..30")
+    }else -> {
+        print("Value is out of range")
+    }
+    }
+}
