5d1ef6895971c93ae1864690e98cade3afe02501
Gerbi <gerbi@protonmail.com>
2018-09-03 20:03:04 -0700

    nestedIf

---
+++ src/SimpleNestedIf.kt
@@ -0,0 +1,17 @@
+fun main(args:Array<String>){
+
+    println("enter your grade: ")
+    var grade= readLine()!!.toDouble()
+
+    if (grade>=90) {
+        //block of code
+        if (grade < 93) {
+            println("You are in A- level")
+        } else {
+            println("You are in A+ level")
+        }
+    }
+        println("You enter grade $grade")
+
+
+}
