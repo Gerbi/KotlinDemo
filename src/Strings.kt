1d3af610f22d094bce9cb1a3c0f4ed50aa7ea9ec
Gerbi <gerbi@protonmail.com>
2018-10-18 16:33:12 -0700

    String

---
+++ src/Strings.kt
@@ -0,0 +1,16 @@
+fun main(args:Array<String>){
+
+    var title = " Welcome to new york"
+    println(title)
+    println("title:"+ title)
+    println("title: $title")
+
+    var name = "Gomez "+"study"
+    println("name: $name")
+    println("Second char:"+title[1])
+    println("title:"+title.toLowerCase())
+    println("title:"+title.toUpperCase())
+
+    println("title:"+title.trim())
+    println("title:"+title.split(" "))
+}
