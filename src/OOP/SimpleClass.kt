50e06616d47fc4b900434b52129763040faa53aa
Gerbi <gerbi@protonmail.com>
2018-10-19 11:26:37 -0700

    scope
    scope

---
+++ src/OOP/SimpleClass.kt
@@ -0,0 +1,14 @@
+package OOP
+
+class Car(){
+    init{
+        println("class is created")
+    }
+}
+
+fun main (args:Array<String>){
+
+    val huCar=Car()
+    val jeCar=Car()
+}
+
---
+++ src/Scope.kt
@@ -0,0 +1,12 @@
+var fullname="gerbi studio" //global var
+fun display():Unit{
+    println(fullname)
+//    println("name:"+ name)
+
+}
+
+fun main (args:Array<String>){
+    var name="gerbi"  //local var
+    println(fullname)
+    println("name:"+ name)
+}
